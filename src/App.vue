bb5c7cf857c96721df6a5c7fc75d5e488e3420d0
Tomas Antecky <tomas@antecky.cz>
2021-08-21 17:46:01 +0200

    Init repo

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <Validator />
+</template>
+
+<script>
+import Validator from './components/Validator.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Validator,
+  },
+}
+</script>
+
+<style>
+*,
+*::after,
+*::before {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  position: relative;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/Validator.vue
@@ -0,0 +1,155 @@
+<template>
+  <div class="container">
+    <h1>
+      <a href="https://en.bitcoin.it/wiki/Invoice_address" target="_blank"
+        >BTC Address</a
+      >
+      Validator
+    </h1>
+    <input
+      type="text"
+      autocomplete="off"
+      placeholder="Paste Here"
+      v-model="inputAddr"
+      @input="check"
+      maxlength="42"
+      :class="[{ valid: isValid }, { invalid: isInvalid }]"
+    />
+    <div class="note">It never leaves your browser</div>
+    <div
+      v-if="this.dirty"
+      class="result"
+      :class="[{ 'valid-text': isValid }, { 'invalid-text': isInvalid }]"
+    >
+      {{ this.msg }}
+    </div>
+    <div class="footer">Version</div>
+  </div>
+</template>
+
+<script>
+import { getAddressInfo } from 'bitcoin-address-validation'
+
+export default {
+  name: 'Validator',
+  data() {
+    return {
+      inputAddr: '',
+      valid: false,
+      dirty: false,
+      msg: '',
+    }
+  },
+  computed: {
+    isValid() {
+      return this.dirty && this.valid
+    },
+    isInvalid() {
+      return this.dirty && !this.valid
+    },
+  },
+  methods: {
+    check() {
+      this.inputAddr = this.inputAddr.replace(/ /g, '')
+      console.log(this.inputAddr)
+
+      if (!this.inputAddr) {
+        this.dirty = false
+        return
+      }
+
+      try {
+        const info = getAddressInfo(this.inputAddr)
+        console.log(info)
+        this.valid = true
+
+        this.msg = 'Valid address'
+      } catch {
+        this.valid = false
+        this.msg = 'Invalid address'
+      } finally {
+        this.dirty = true
+      }
+    },
+  },
+}
+</script>
+
+<style>
+a {
+  color: #0d6efd;
+  text-decoration: underline;
+}
+
+h1 {
+  font-size: 36px;
+  margin-bottom: 8px;
+}
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 100vh;
+  width: 100vw;
+}
+
+input {
+  font-size: 32px;
+  max-width: 750px;
+  width: 750px;
+  text-align: center;
+  line-height: 1.5;
+  color: #495057;
+  background-color: #fff;
+  background-clip: padding-box;
+  border: 1px solid #ced4da;
+  border-radius: 0.25rem;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+}
+
+input:focus {
+  color: #495057;
+  background-color: #fff;
+  border-color: #80bdff;
+  outline: 0;
+  box-shadow: 0 0 0 0.2rem rgb(0 123 255 / 25%);
+}
+
+.note {
+  margin-top: 4px;
+  font-size: 14px;
+  color: #ccc;
+}
+
+.result {
+  font-size: 32px;
+  position: absolute;
+  font-weight: bold;
+  margin-top: 180px;
+}
+
+.valid {
+  color: #28a745 !important;
+  border-color: #28a745 !important;
+  box-shadow: 0 0 0 0.2rem rgb(40 167 69/ 25%) !important;
+}
+
+.valid-text {
+  color: #28a745 !important;
+}
+
+.invalid {
+  color: #dc3545 !important;
+  border-color: #dc3545 !important;
+  box-shadow: 0 0 0 0.2rem rgb(220 53 69/ 25%) !important;
+}
+
+.invalid-text {
+  color: #dc3545 !important;
+}
+.footer {
+  position: absolute;
+  bottom: 40px;
+}
+</style>
